f347e126bb46f8dbf3b8c16f217b5ef34eb7853f
lankmar <32324313+lankmar@users.noreply.github.com>
2023-07-18 11:54:08 +0300

    Home work 2

---
+++ Assets/Scripts/UserControlSystem/CommandsRealization/PatrolCommand.cs
@@ -0,0 +1,14 @@
+﻿using Abstractions.Commands;
+using Abstractions.Commands.CommandsInterfaces;
+using UnityEngine;
+
+namespace UserControlSystem.CommandsRealization
+{
+    public sealed class PatrolCommand : CommandExecutorBase<IPatrolCommand>
+    {
+        public override void ExecuteSpecificCommand(IPatrolCommand command)
+        {
+            UnityEngine.Debug.Log("Patrol");
+        }
+    }
+}
